eac993811f4bf9d549d7429b6b85481cf1476e55
Chris Lee <sirleech@gmail.com>
2018-05-15 13:51:36 +1000

    add DSS pages

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const data =
     { name: "privacystatement", path: "/privacy-statement/" },
     { name: "dss",              path: "/digital-service-standard/" },
     { name: "dss1",             path: "/digital-service-standard/1-understand-user-needs/" },
+    { name: "dss2",             path: "/digital-service-standard/2-multidisciplinary-team/" },
+    { name: "dss3",             path: "/digital-service-standard/3-agile-and-user-centred/" },
+    { name: "dss4",             path: "/digital-service-standard/4-tools-and-systems/" },
+    { name: "dss5",             path: "/digital-service-standard/5-make-it-secure/" },
+    { name: "dss6",             path: "/digital-service-standard/6-consistent-and-responsive/" },
+    { name: "dss7",             path: "/digital-service-standard/7-open-standards-and-common-platforms/" },
+    { name: "dss8",             path: "/digital-service-standard/8-make-source-code-open/" },
+    { name: "dss9",             path: "/digital-service-standard/9-make-it-accessible/" },
+    { name: "dss10",            path: "/digital-service-standard/10-test-the-service/" },
+    { name: "dss11",            path: "/digital-service-standard/11-measure-performance/" },
+    { name: "dss12",            path: "/digital-service-standard/12-non-digital-experience/" },
+    { name: "dss13",            path: "/digital-service-standard/13-encourage-use-of-the-digital-service/" }
   ]
 }
 
