66e63520ba4d768604ab4b82df946756b112db0b
Chris Lee <sirleech@gmail.com>
2018-05-15 13:35:07 +1000

    preserve example

---
+++ examples/screenshot-bunchofpages.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer');
+
+// macbook pro 15"
+const viewport = {width:1440,height:1080};
+
+// the site data
+const data = 
+{
+  "pages": [
+    {"name":"index","path":"/"},
+    {"name":"sitemap","path":"/sitemap/"},
+    {"name":"privacystatement","path":"/privacy-statement/"}
+  ]
+}
+
+// make the output directory if it's not there
+const fs = require('fs');
+const dir = 'output/';
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+// puppeteer routine
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  page.setViewport(viewport);
+
+  // login u: guides/p: guides staging password
+  await page.authenticate({username:"guides", password:"guides"});
+
+  // set the domain (test,stage,prod)
+  // do not add a trailing slash
+  const env = 'https://service-manual-test-staging.apps.y.cld.gov.au';
+
+  //////////////////////////////////////////////////
+  //
+  //     Screenshot every published page  📸 
+  //
+  //////////////////////////////////////////////////
+
+  for (let i = 0; i < data.pages.length; i++) {
+    await page.goto(env + data.pages[i].path);
+    await page.screenshot({path: 'output/'+ i +'-'+ data.pages[i].name + '.png',fullPage: true});
+    console.log("📸 Snapped a screenshot of " + env + data.pages[i].path);
+  }
+
+
+
+  //////////////////////////////////////////////////
+  await browser.close();
+})();
