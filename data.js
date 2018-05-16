8ea1117ff30c2b04ce6518291a000c3102263082
Chris Lee <sirleech@gmail.com>
2018-05-16 10:23:18 +1000

    Formatting the data file

--- data.js
+++ data.js
@@ -7,23 +7,24 @@ module.exports = {
       {
         pages: [
 
-{ name: "index",            path: "/" },
-{ name: "sitemap",          path: "/sitemap/" },
+{ name: "index", path: "/" },
+{ name: "sitemap", path: "/sitemap/" },
 { name: "privacystatement", path: "/privacy-statement/" },
-{ name: "dss",              path: "/digital-service-standard/" },
-{ name: "dss1",             path: "/digital-service-standard/1-understand-user-needs/" },
-{ name: "dss2",             path: "/digital-service-standard/2-multidisciplinary-team/" },
-{ name: "dss3",             path: "/digital-service-standard/3-agile-and-user-centred/" },
-{ name: "dss4",             path: "/digital-service-standard/4-tools-and-systems/" },
-{ name: "dss5",             path: "/digital-service-standard/5-make-it-secure/" },
-{ name: "dss6",             path: "/digital-service-standard/6-consistent-and-responsive/" },
-{ name: "dss7",             path: "/digital-service-standard/7-open-standards-and-common-platforms/" },
-{ name: "dss8",             path: "/digital-service-standard/8-make-source-code-open/" },
-{ name: "dss9",             path: "/digital-service-standard/9-make-it-accessible/" },
-{ name: "dss10",            path: "/digital-service-standard/10-test-the-service/" },
-{ name: "dss11",            path: "/digital-service-standard/11-measure-performance/" },
-{ name: "dss12",            path: "/digital-service-standard/12-non-digital-experience/" },
-{ name: "dss13",            path: "/digital-service-standard/13-encourage-use-of-the-digital-service/" }
+
+{ name: "dss00", path: "/digital-service-standard/" },
+{ name: "dss01", path: "/digital-service-standard/1-understand-user-needs/" },
+{ name: "dss02", path: "/digital-service-standard/2-multidisciplinary-team/" },
+{ name: "dss03", path: "/digital-service-standard/3-agile-and-user-centred/" },
+{ name: "dss04", path: "/digital-service-standard/4-tools-and-systems/" },
+{ name: "dss05", path: "/digital-service-standard/5-make-it-secure/" },
+{ name: "dss06", path: "/digital-service-standard/6-consistent-and-responsive/" },
+{ name: "dss07", path: "/digital-service-standard/7-open-standards-and-common-platforms/" },
+{ name: "dss08", path: "/digital-service-standard/8-make-source-code-open/" },
+{ name: "dss09", path: "/digital-service-standard/9-make-it-accessible/" },
+{ name: "dss10", path: "/digital-service-standard/10-test-the-service/" },
+{ name: "dss11", path: "/digital-service-standard/11-measure-performance/" },
+{ name: "dss12", path: "/digital-service-standard/12-non-digital-experience/" },
+{ name: "dss13", path: "/digital-service-standard/13-encourage-use-of-the-digital-service/" }
 
         ]
       }
